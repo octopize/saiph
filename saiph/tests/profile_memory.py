1f00931f575086f49e50861470e4af575c8fed89
Charles-Axel Dein <120501+charlax@users.noreply.github.com>
2022-03-15 18:50:49 +0100

    test: profile only fit code

--- saiph/tests/profile_memory.py
+++ saiph/tests/profile_memory.py
@@ -4,11 +4,15 @@ import tracemalloc
 import numpy as np
 import pandas as pd
 # from scalene import scalene_profiler
+from filprofiler.api import profile
 
 from saiph import fit
 from saiph.lib.size import get_readable_size
 
 N_ROWS = 10000
+
+# Run with:
+# poetry run fil-profile python saiph/tests/profile_memory.py
 
 
 def main() -> None:
@@ -22,7 +26,7 @@ def main() -> None:
     # scalene_profiler.start()
     print("before fit")
     start = time.perf_counter()
-    fit(df)
+    profile(lambda: fit(df), './fil-result')
     end = time.perf_counter()
 
     print(f"after fit, took {(end-start):.3} sec")

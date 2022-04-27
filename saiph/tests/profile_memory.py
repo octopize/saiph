7478be5d2b5d6aee31deb8786b5fbfacfd9f928c
Julien Petot <48205834+jpetot@users.noreply.github.com>
2022-04-27 11:27:53 +0200

    feat: memory profile

---
+++ saiph/tests/profile_memory.py
@@ -0,0 +1,57 @@
+import time
+import webbrowser
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from filprofiler.api import profile
+
+from saiph import fit
+from saiph.lib.size import get_readable_size
+
+# flake8: noqa
+
+N_ROWS = 10000
+
+# Run with:
+# poetry run fil-profile python saiph/tests/profile_memory.py
+
+BASE_PATH = (Path(__file__).parent / "../../").resolve()
+
+
+def main() -> None:
+    quali = np.random.randint(0, 1000, size=N_ROWS).astype(object)
+    quanti = np.random.randint(0, 1000, size=N_ROWS, dtype=np.int32)
+
+    df = pd.DataFrame({"quali": quali, "quanti": quanti})
+    print(f"using {get_readable_size(df.memory_usage(index=True).sum())}")
+
+    print("before fit")
+    start = time.perf_counter()
+    # fit(df, nf=5)
+    filename = f"fil-result/{time.time()}"
+    full_path = BASE_PATH / filename / "index.html"
+    print(full_path)
+    profile(lambda: fit(df, nf=5), filename)
+    end = time.perf_counter()
+
+    print(f"after fit, took {(end-start):.3} sec")
+
+    webbrowser.open(f"file://{str(full_path)}")
+
+    print("before fit 2")
+    start = time.perf_counter()
+    # fit(df, nf=5)
+    filename = f"fil-result/{time.time()}"
+    full_path = BASE_PATH / filename / "index.html"
+    print(full_path)
+    profile(lambda: fit(df, nf=5, sparse=True), filename)
+    end = time.perf_counter()
+
+    print(f"after fit, took {(end-start):.3} sec")
+
+    webbrowser.open(f"file://{str(full_path)}")
+
+
+if __name__ == "__main__":
+    main()

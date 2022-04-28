a41aef79ac783c707ded47af3463b8f274ec995c
Julien Petot <48205834+jpetot@users.noreply.github.com>
2022-04-28 15:46:59 +0200

    refactor: error message

--- saiph/reduction/utils/check_params.py
+++ saiph/reduction/utils/check_params.py
@@ -9,10 +9,12 @@ def fit_check_params(nf: int, col_w: NDArray[Any], df: pd.DataFrame) -> None:
         raise ValueError("nf", "The number of components must be positive.")
 
     if nf > min(pd.get_dummies(df).shape):
-        raise ValueError(f"Expected number of components <= {min(pd.get_dummies(df).shape)}, got {nf} instead.")
+        raise ValueError(
+            f"Expected number of components <= {min(pd.get_dummies(df).shape)}, got {nf} instead."
+        )
 
     if len(col_w) != df.shape[1]:
         raise ValueError(
             "col_w",
-            f"The weight parameter should be of size {str(df.shape[1])}.",
+            f"Expected weight parameter size {str(df.shape[1])}, got {len(col_w)} instead.",
         )

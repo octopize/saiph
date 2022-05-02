058278632983bbc172653a4ddae5f1930c349955
Julien Petot <48205834+jpetot@users.noreply.github.com>
2022-05-02 11:31:21 +0200

    test: add memory test

---
+++ saiph/tests/benchmark_test.py
@@ -0,0 +1,22 @@
+import tracemalloc
+from pathlib import Path
+from typing import Any
+
+import pandas as pd
+
+from saiph.projection import fit
+
+
+def test_memory_iris(record_property: Any) -> None:
+    path = (Path(__file__) / "../../../fixtures/iris.csv").resolve()
+    iris_df = pd.read_csv(path)
+
+    tracemalloc.start()
+    fit(iris_df)
+
+    _, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+
+    # memory usage should be below x kiB
+    assert peak < 360000
+    record_property("peak_memory_usage", peak)

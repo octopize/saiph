a7b082689000268f8b53bef0950fd54e9737d0f1
Julien Petot <48205834+jpetot@users.noreply.github.com>
2022-05-02 12:29:23 +0200

    fix: determinist memory test

--- saiph/tests/benchmark_test.py
+++ saiph/tests/benchmark_test.py
@@ -6,17 +6,15 @@ import pandas as pd
 
 from saiph.projection import fit
 
+from resource import getrusage, RUSAGE_SELF
+
 
 def test_memory_iris(record_property: Any) -> None:
     path = (Path(__file__) / "../../../fixtures/iris.csv").resolve()
     iris_df = pd.read_csv(path)
-
-    tracemalloc.start()
+  
     fit(iris_df)
-
-    _, peak = tracemalloc.get_traced_memory()
-    tracemalloc.stop()
-
+    peak = int(getrusage(RUSAGE_SELF).ru_maxrss / 1024)
     # memory usage should be below x kiB
-    assert peak <= 64276
+    assert peak <= 109872 * 1.01
     record_property("peak_memory_usage", peak)

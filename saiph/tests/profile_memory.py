25e8a99e11c5242cbb920371f14b12b57eae0875
tas17 <35601762+tas17@users.noreply.github.com>
2022-05-05 15:22:37 +0200

    fix: vulnerabilities of tmp

--- saiph/tests/profile_memory.py
+++ saiph/tests/profile_memory.py
@@ -15,18 +15,18 @@ N_ROWS = 10000
 # Run with:
 # poetry run fil-profile python saiph/tests/profile_memory.py True
 
-BASE_PATH = (Path(__file__).parent / "../").resolve()
+BASE_PATH = (Path(__file__).parent / "../../").resolve()
 
 
 def main(sparse: bool) -> None:
     """Profile famd.fit using a fake dataset."""
-    df = pd.read_csv(str(BASE_PATH) + "/../tmp/fake_1000000.csv")
+    df = pd.read_csv(str(BASE_PATH / "tmp/fake_1000000.csv"))
 
     typer.echo(f"using {get_readable_size(df.memory_usage(index=True).sum())}")
 
     typer.echo("before fit")
     start = time.perf_counter()
-    filename = f"/tmp/{time.time()}"
+    filename = f"tmp/{time.time()}"
     full_path = BASE_PATH / filename / "index.html"
     profile(lambda: fit(df, nf=5, sparse=sparse), filename)
     end = time.perf_counter()

dd6933be52d38fcbe0a7fa4930da89787848f463
Julien Petot <48205834+jpetot@users.noreply.github.com>
2022-05-12 12:05:14 +0200

    refactor: move test

--- saiph/inverse_transform_test.py
+++ saiph/inverse_transform_test.py
@@ -5,7 +5,12 @@ import pandas as pd
 import pytest
 from pandas.testing import assert_frame_equal
 
-from saiph.inverse_transform import get_random_weighted_columns, undummify
+from saiph.inverse_transform import (
+    get_random_weighted_columns,
+    inverse_transform,
+    undummify,
+)
+from saiph.projection import fit_transform
 
 
 @pytest.mark.parametrize(
@@ -44,6 +49,7 @@ def test_get_random_weighted_columns(weights: List[float], expected_index: int) 
 def test_undummify(
     mapping: Dict[str, List[str]], use_max_modalities: bool, expected: pd.DataFrame
 ) -> None:
+    """Test undummify a disjunctive table with different use_max_modalities."""
     dummy_df = pd.DataFrame(
         [[0.3, 0.7, 0.01, 0.99], [0.51, 0.49, 0.8, 0.2]],
         columns=["tool___hammer", "tool___wrench", "fruit___apple", "fruit___orange"],
@@ -52,3 +58,54 @@ def test_undummify(
     df = undummify(dummy_df, mapping, use_max_modalities=use_max_modalities, seed=321)
 
     assert_frame_equal(df, expected)
+
+
+# wider than len df
+def test_inverse_transform_raises_value_error_when_wider_than_df() -> None:
+    wider_df = pd.DataFrame(
+        {
+            "variable_1": ["a", "b", "c"],
+            "variable_2": ["ZZ", "ZZ", "WW"],
+        }
+    )
+    coord, model = fit_transform(wider_df)
+    with pytest.raises(ValueError, match=r"n_dimensions"):
+        inverse_transform(coord, model)
+
+
+# using df with more dimensions than individuals and high column weights
+# allows for a more balanced probability in modality assignment during inverse transform
+
+
+def test_inverse_transform_with_ponderation() -> None:
+    """Verify that use_max_modalities=False returns a random ponderation of modalities."""
+    df = pd.DataFrame(
+        zip(["a", "b", "c"], ["ZZ", "ZZ", "WW"], [1, 2, 3], [2, 2, 10]),
+        columns=["cat1", "cat2", "cont1", "cont2"],
+    )
+    inverse_expected = pd.DataFrame(
+        zip(["c", "b", "a"], ["ZZ", "ZZ", "WW"], [1, 2, 2], [4, 4, 4]),
+        columns=["cat1", "cat2", "cont1", "cont2"],
+    )
+    coord, model = fit_transform(df, col_w=np.array([1, 2000, 1, 1]))
+    result = inverse_transform(
+        coord, model, use_approximate_inverse=True, use_max_modalities=False, seed=46
+    )
+    assert_frame_equal(result, inverse_expected)
+
+
+def test_inverse_transform_deterministic() -> None:
+    """Verify that use_max_modalities=True returns a deterministic of modalities."""
+    df = pd.DataFrame(
+        zip(["a", "b", "c"], ["ZZ", "ZZ", "WW"], [1, 2, 3], [2, 2, 10]),
+        columns=["cat1", "cat2", "cont1", "cont2"],
+    )
+    inverse_expected = pd.DataFrame(
+        zip(["a", "b", "c"], ["ZZ", "ZZ", "WW"], [1, 2, 2], [4, 4, 4]),
+        columns=["cat1", "cat2", "cont1", "cont2"],
+    )
+    coord, model = fit_transform(df, col_w=np.array([1, 2000, 1, 1]))
+    result = inverse_transform(
+        coord, model, use_approximate_inverse=True, use_max_modalities=True, seed=46
+    )
+    assert_frame_equal(result, inverse_expected)

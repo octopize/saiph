7f9c651b9996a523768b63fab0c9ce307e21855d
Tom Crasset <25140344+tcrasset@users.noreply.github.com>
2022-05-30 10:05:44 +0200

    chore: run linter

--- saiph/projection_test.py
+++ saiph/projection_test.py
@@ -357,21 +357,27 @@ def test_get_variable_contributions_sum_is_100_famd(mixed_df: pd.DataFrame) -> N
     assert_series_equal(summed_contributions, pd.Series([100.0] * 3), check_index=False)
 
 
-def test_get_variable_contributions_sum_is_100_with_col_weights_1_famd(mixed_df: pd.DataFrame) -> None:
+def test_get_variable_contributions_sum_is_100_with_col_weights_1_famd(
+    mixed_df: pd.DataFrame,
+) -> None:
     model = fit(mixed_df, col_weights=[1.0, 1.0])
     contributions = projection.get_variable_contributions(model, mixed_df)
     summed_contributions = contributions.sum(axis=0)
     assert_series_equal(summed_contributions, pd.Series([100.0] * 3), check_index=False)
 
 
-def test_get_variable_contributions_sum_is_100_with_col_weights_random_famd(mixed_df: pd.DataFrame) -> None:
+def test_get_variable_contributions_sum_is_100_with_col_weights_random_famd(
+    mixed_df: pd.DataFrame,
+) -> None:
     model = fit(mixed_df, col_weights=[3.0, 2.0])
     contributions = projection.get_variable_contributions(model, mixed_df)
     summed_contributions = contributions.sum(axis=0)
     assert_series_equal(summed_contributions, pd.Series([100.0] * 3), check_index=False)
 
 
-def test_get_variable_contributions_sum_is_100_with_col_weights_random_famd2(iris_df: pd.DataFrame) -> None:
+def test_get_variable_contributions_sum_is_100_with_col_weights_random_famd2(
+    iris_df: pd.DataFrame,
+) -> None:
     model = fit(iris_df, col_weights=[3.0, 2.0, 0.5, 1.0, 2.1])
     contributions = projection.get_variable_contributions(model, iris_df)
     summed_contributions = contributions.sum(axis=0)
@@ -385,14 +391,18 @@ def test_get_variable_contributions_sum_is_100_mca(quali_df: pd.DataFrame) -> No
     assert_series_equal(summed_contributions, pd.Series([100.0] * 4), check_index=False)
 
 
-def test_get_variable_contributions_sum_is_100_with_col_weights_1_mca(quali_df: pd.DataFrame) -> None:
+def test_get_variable_contributions_sum_is_100_with_col_weights_1_mca(
+    quali_df: pd.DataFrame,
+) -> None:
     model = fit(quali_df, col_weights=[1.0, 1.0])
     contributions = projection.get_variable_contributions(model, quali_df)
     summed_contributions = contributions.sum(axis=0)
     assert_series_equal(summed_contributions, pd.Series([100.0] * 4), check_index=False)
 
 
-def test_get_variable_contributions_sum_is_100_with_col_weights_random_mca(quali_df: pd.DataFrame) -> None:
+def test_get_variable_contributions_sum_is_100_with_col_weights_random_mca(
+    quali_df: pd.DataFrame,
+) -> None:
     model = fit(quali_df, col_weights=[3.0, 2.0])
     contributions = projection.get_variable_contributions(model, quali_df)
     summed_contributions = contributions.sum(axis=0)

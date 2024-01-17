47de3ca26403cf08100d5fa79d8ebcd72cf06c46
Tom Crasset <25140344+tcrasset@users.noreply.github.com>
2024-01-17 15:24:54 +0100

    chore: release version 1.5.0

--- saiph/__init__.py
+++ saiph/__init__.py
@@ -2,7 +2,7 @@ from .inverse_transform import inverse_transform
 from .projection import fit, fit_transform, stats, transform
 
 # Also modify in pyproject.toml
-__version__ = "1.4.7"
+__version__ = "1.5.0"
 
 __all__ = [
     "__version__",

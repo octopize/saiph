21c422396b4aa40998a35667a060938c765c966f
Lucie R <101066918+raimbaultL@users.noreply.github.com>
2024-07-19 10:34:32 +0200

    feat(projections): add an error on different columns between transform and model

--- saiph/exception.py
+++ saiph/exception.py
@@ -2,3 +2,9 @@ class InvalidParameterException(Exception):
     """Error encountered when an invalid parameter is passed."""
 
     pass
+
+
+class NotFoundColumnsError(Exception):
+    """Error encountered when columns are not found in the dataframe."""
+
+    pass
